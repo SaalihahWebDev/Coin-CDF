d53ac2e1d58cdeba050aea8500c7efa9f8d90394
SaalihahWebDev <bashirabanu91@gmail.com>
2025-09-29 05:19:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
 coins=["H","T"]
 outcomes=list(itertools.product(coins,repeat=6))
+total=len(outcomes)
 for outcome in outcomes:
     print(f"Probability of {outcome}=1/{total}={1/total}")
